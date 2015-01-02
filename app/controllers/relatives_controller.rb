9a5345043f11a544337b5eb80eb1d3b29ee65077
Rebecca Mark <rebecca.lmark@gmail.com>
2015-01-02 00:16:23 -0800

    app no longer displays all family info to all users

--- app/controllers/relatives_controller.rb
+++ app/controllers/relatives_controller.rb
@@ -1,14 +1,26 @@
 class RelativesController < ApplicationController
   def new
+    # Join table
+    @relation = Relation.new
+
+    # Three column layout
     @relative = Relative.new
     @decks_relative = Relative.where(deck_id: session[:deck_id])
-    @relatives = Relative.all
+
+    relations = Relation.where(familymember_id: current_user.relative_id)
+    @relatives = relations.collect do |join|
+      join.relative
+    end
   end
 
   def create
     @relative = Relative.new(relative_params)
     @relative.deck_id = session[:deck_id]
     if @relative.save
+      # Familymember id = the user
+      relation = @relative.relations.new(relation_params)
+      relation.familymember = current_user.relative
+      relation.save
       redirect_to new_relative_path
     else
       render :new
@@ -34,5 +46,9 @@ class RelativesController < ApplicationController
 
   def relative_params
     params.require(:relative).permit(:firstname, :lastname, :notes, :pronouns, :likes, :dislikes)
+  end
+
+  def relation_params
+    params.require(:relation).permit(:title, :divorced, :step)
   end
 end
--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,4 +1,6 @@
 class Relation < ActiveRecord::Base
   belongs_to :relative
   belongs_to :familymember, class_name: "Relative"
+
+  TITLES = %w(Mother Father Sister Brother Uncle Aunt Cousin Grandmother Grandfather Great-aunt Great-uncle Great-grandfather Great-grandmother )
 end
