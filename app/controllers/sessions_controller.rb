97820f73dc78ab072b334385e578c13921c7f419
Rebecca Mark <rebecca.lmark@gmail.com>
2015-01-03 02:46:24 -0800

    fixed double render of flash notice on incorrect sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
-      flash.notice = "Invalid email or password"
+      flash.alert = "Invalid email or password"
       @user = User.new
       render "welcome/index"
     end
